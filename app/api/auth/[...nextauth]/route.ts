b8559d47644d627ab5621fb3845c684fcf13a47f
omonbek <jarimjonovomonbek@gmail.com>
2025-07-02 17:26:43 +0500

    10dars

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth"; // 👈 quyidagi faylda sozlanadi
+import { authOptions } from "@/lib/auth"; 
 
 const handler = NextAuth(authOptions);
 
